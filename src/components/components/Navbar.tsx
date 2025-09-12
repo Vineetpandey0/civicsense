7e32c3bd37ec795d15756f5307443b410261bee0
Vineet Pandey <vineetpandey0010@gmail.com>
2025-09-13 02:49:55 +0530

    minor bug fix

--- src/components/components/Navbar.tsx
+++ src/components/components/Navbar.tsx
@@ -9,18 +9,12 @@ import { usePathname } from "next/navigation"  // for active route
 
 function Navbar() {
   const pathname = usePathname()
-  const token = localStorage.getItem("auth-storage")
-  const isLoggedIn = token && token !== "undefined" && token !== "null"
 
-  
-  let navItems:any = []
-  if (isLoggedIn) {
-    navItems = [
-      { href: "/dashboard", label: "Dashboard" },
-      { href: "/complaints", label: "Complaints" },
-      { href: "/profile", label: "Profile" },
-    ]
-  }
+  const navItems = [
+    { href: "/dashboard", label: "Dashboard" },
+    { href: "/complaints", label: "Complaints" },
+    { href: "/profile", label: "Profile" },
+  ]
 
   return (
     <header className="flex items-center justify-between whitespace-nowrap border-b border-gray-200 px-6 py-6 bg-white shadow-sm">
@@ -32,7 +26,7 @@ function Navbar() {
 
       {/* Desktop Nav */}
       <nav className="hidden sm:flex items-center gap-6 text-md font-medium">
-        {navItems.map((item:any) => (
+        {navItems.map((item) => (
           <Link
             key={item.href}
             href={item.href}
@@ -59,7 +53,7 @@ function Navbar() {
             <SheetTitle className="text-lg font-bold text-gray-900">Menu</SheetTitle>
           </SheetHeader>
           <nav className="mt-4 flex flex-col gap-3 text-gray-700">
-            {navItems.map((item:any) => (
+            {navItems.map((item) => (
               <Link
                 key={item.href}
                 href={item.href}
